0c6f35e3ad30200b8f0aecbdcc344fa6cb00a008
TiagoOliveiraGomes <TyagooGomes@gmail.com>
2020-11-09 15:34:15 -0300

    asd

---
+++ frontend/src/about/About.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import PageHeader from '../template/PageHeader'
+
+export default (props) => {
+  return (
+    <div>
+      <PageHeader name='Sobre' small='Nós'></PageHeader>
+      <h2>Nossa História</h2>
+      <p>Lorem djdjjd akkasd  fdf</p>
+      <h2>Missão e Visão</h2>
+      <p>asdasdasfsd ds gfdsg  sdgf s</p>
+      <h2>Imprensa</h2>
+      <p>akk f kfdk s lsl lfld </p>
+    </div>
+  );
+};
